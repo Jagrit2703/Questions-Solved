b40b84b3d620ed32666ea33cd23eee0423fdf690
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-05-20 23:19:31 +0530

    Time: 9 ms (71.82%) | Memory: 7.3 MB (52.71%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    if(s.length()!=t.length())
-    return false;
-    sort(s.begin(),s.end());
-    sort(t.begin(),t.end());
-    if (s==t) return true;
-    return false;
+        unordered_map<char,int>sm,tm;
+        for(char c:s){
+            sm[c]++;
+        }
+        for(char c:t){
+            tm[c]++;
+        }
+        if(s.length()!=t.length()){
+            return false;
+        }
+            for(auto pair:sm){
+                char c=pair.first;
+                int freq=pair.second;
+                if(tm[c]!=freq)return false;
+            }
+        return true;
     }
 };
