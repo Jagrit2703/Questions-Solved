7685b0eaf7f56490f027281af4f77dacb282be19
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-06-10 22:48:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (39.94%) - LeetSync

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -1,20 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        if(!root) return true;
-        stack<TreeNode*> st;
-        st.push(root->left);
-        st.push(root->right);
-        while(!st.empty()){
-            TreeNode* q = st.top(); st.pop();
-            TreeNode* p = st.top(); st.pop();
-            if(!p && !q) continue;
-            if(!p || !q || p->val != q->val) return false;
-            st.push(p->left);
-            st.push(q->right);
-            st.push(p->right);
-            st.push(q->left);
-        }
-        return true;
+    bool isTreeSymmetric(TreeNode* leftroot,TreeNode* rightroot) {
+        if(leftroot==nullptr&&rightroot==nullptr)return true;
+        if(leftroot==nullptr || rightroot==nullptr)return false;
+        if(leftroot->val != rightroot->val)return false;
+        return isTreeSymmetric(leftroot->left,rightroot->right)&& isTreeSymmetric(leftroot->right , rightroot->left);
+    }
+    bool isSymmetric(TreeNode* root){
+        return isTreeSymmetric(root->left,root->right);
     }
 };
