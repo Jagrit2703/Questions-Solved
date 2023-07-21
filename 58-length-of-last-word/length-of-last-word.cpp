7200b7e8f034b447dfb4115d38a4ddd90b75aba5
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-07-21 23:08:30 +0530

    Time: 5 ms (39.14%) | Memory: 6.6 MB (38.88%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-int lengthOfLastWord(string s) {
-    int count = 0;
-    int i = s.length() - 1;
-    while (i >= 0 && s[i] == ' ') {
-        i--;
+    int lengthOfLastWord(string s) {
+        int siz=s.size(),kount=0,flag=0;
+        for(int i=siz-1;i>=0;i--){
+            if(s[i]==' '&&flag)break;
+            if(s[i]!=' '){
+                flag=1;
+                kount++;
+            }
+        }
+        return kount;
     }
-    while (i >= 0 && s[i] != ' ') {
-        count++;
-        i--;
-    }
-    return count;
-
-}
 };
