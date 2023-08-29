b71485c1d524e9ed16ea9591220cfc20c48790cf
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-08-29 09:06:47 +0530

    Time: 16 ms (98.58%) | Memory: 10.7 MB (72.59%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int minPenalty = 0, curPenalty = 0;
-        int earliestHour = 0;
-
-        for (int i = 0; i < customers.size(); i++) {
-            char ch = customers[i];
-            if (ch == 'Y') {
-                curPenalty--;
-            } else {
-                curPenalty++;
-            }
-            if (curPenalty < minPenalty) {
-                earliestHour = i + 1;
-                minPenalty = curPenalty;
+        int count = 0;
+        int min = count,index = 0;
+        for(int i=1;i<=customers.length();i++){
+            if(customers[i-1]=='Y') count--;
+            else count++;
+            if(count<min){
+                min = count;
+                index = i;
             }
         }
-
-        return earliestHour;
+        return index;
     }
 };
