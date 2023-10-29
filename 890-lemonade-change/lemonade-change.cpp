d9b6ce50a0ba4505ac5906889f04bd04302d9d8e
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-10-29 22:00:16 +0530

    Time: 89 ms (16.80%) | Memory: 83.7 MB (67.11%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0;
+        int ten=0;
+        for (int i = 0; i < bills.size(); ++ i) {
+        switch(bills[i]) {
+            case 5: 
+                five++;
+                break;
+            case 10:                     
+                five--;
+                ten++;
+                break;
+            case 20: 
+                if(ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                } 
+        }
+        if(five < 0) return false;
+    }
+    return true;
+    }
+};
