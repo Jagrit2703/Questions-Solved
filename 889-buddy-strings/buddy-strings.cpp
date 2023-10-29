42f43b64ccaa2d4ed6bea478893db6c5c62e9b0c
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-10-29 22:41:19 +0530

    Time: 5 ms (54.31%) | Memory: 7.3 MB (35.07%) - LeetSync

--- 889-buddy-strings/buddy-strings.cpp
+++ 889-buddy-strings/buddy-strings.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    bool buddyStrings(string s, string goal) {
-        if(s.length()!=goal.length())return false;
-        unordered_map<char,int>mp;
-        for(int i =0; i<s.length(); i++) mp[s[i]] = i;
-        for(int i =0; i<goal.length(); i++){
-            if(mp.find(goal[i]) == mp.end()) return false;
-            if(mp[goal[i]] != i){
-                swap(s[i], s[mp[goal[i]]]);
-                if(s == goal) return true;
-                swap(s[i], s[mp[goal[i]]]);
+    bool checker(string& s){
+        int arr[26]={0};
+        for(char &ch:s){
+            arr[ch-'a']++;
+            if(arr[ch-'a']>1){
+                return true;
             }
         }
         return false;
+    }
+    bool buddyStrings(string s, string goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        if(s==goal){
+            return checker(s);
+        }
+        vector<int>index;
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=goal[i]){
+                index.push_back(i);
+            }
+        }
+        if(index.size()!=2){
+            return false;
+        }
+        swap(s[index[0]],s[index[1]]);
+        return s==goal;
     }
 };
