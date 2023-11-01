ba2393a9255e5f1f68eb6885bda01ca5352bb6c1
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-11-01 23:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (5.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if(n<2){
+            return n;
+        }
         vector<int> v(n+1,0);
         v[0] = v[1] = 1;
-
         for(int c1=2;c1<=n;c1++){
             v[c1] = v[c1-1]+v[c1-2];
         }
