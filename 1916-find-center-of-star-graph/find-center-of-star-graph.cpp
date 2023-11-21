7b5a4f4dabae13c47a9c993a92c5c608a7338a91
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-11-21 22:30:58 +0530

    Time: 249 ms (10.51%) | Memory: 105.9 MB (5.41%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int , set<int> > adj;
+        unordered_map<int,vector<int>>adjList;
         int maxi=0;
-        for(int i = 0 ; i < edges.size() ; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            maxi = max(maxi ,max(u,v));
-
-            adj[u].insert(v);
-            adj[v].insert(u);
+        for(int it=0; it<edges.size();it++){
+            int u=edges[it][0];   //i guess substitute for edges[it].first
+            int v=edges[it][1];   //similarly for edges[it].second
+            maxi=max(maxi,max(u,v));
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
         }
-
-        for(auto i : adj){
-            if(i.second.size() == maxi-1)
-                return i.first;
+        for(auto i:adjList){
+            if(i.second.size()==maxi-1) return i.first;
         }
         return -1;
     }
