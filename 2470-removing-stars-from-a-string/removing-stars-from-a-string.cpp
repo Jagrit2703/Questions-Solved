b668d564b071b30dc678f264f66acf3f1a686dca
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-12-23 19:46:08 +0530

    Time: 80 ms (87.04%) | Memory: 27.3 MB (37.25%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string ans = ""; 
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] == '*') ans.pop_back();
-            else ans.push_back(s[i]);
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty()){
+                if(s[i] != '*') st.push(s[i]);
+            }
+            else if(s[i] != '*') st.push(s[i]);
+            else st.pop();
         }
+        string ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
