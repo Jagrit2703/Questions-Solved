581cb96dcc09f6c1d20870d46cdf95b979b3af1f
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-01-09 21:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (42.46%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,14 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    int hIndex(vector<int>& citations) {
-        int maxi = 0;
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.begin(), citations.end());
         int n = citations.size();
-        sort(citations.begin(),citations.end());
-        for(int i = 0; i < n; i++){
-            if(citations[i] >= n-i){
-                maxi = max(maxi,n-i);
+        int start = 0, end = n - 1;
+        int ans = 0;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (citations[mid] >= n - mid)
+            {
+                ans = max(ans, n - mid);
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
             }
         }
-        return maxi;
+        return ans;
     }
 };
