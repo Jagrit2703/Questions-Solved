6b46c08b27e875509d74e9d91ae89eb459d400a3
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-01-30 00:03:02 +0530

    Time: 16 ms (40.75%) | Memory: 22.5 MB (93.98%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    void preOrderTraversal(TreeNode* root, vector<int> &v){
-        if(root == NULL)    
-        return;
-        v.push_back(root->val);
-        preOrderTraversal(root->left, v);
-        preOrderTraversal(root->right, v);      
+    void solve(TreeNode* root, int &cnt, int &ans, int k){
+        if(root == NULL)    return;
+        //left, root, right 
+        solve(root->left, cnt, ans, k);
+        cnt++;
+        if(cnt == k){
+            ans = root->val;
+            return;
+        }
+        solve(root->right, cnt, ans, k);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> v; 
-        preOrderTraversal(root, v);
-        sort(v.begin(), v.end());
-        return v[k-1];
+        
+        int cnt = 0;        
+        int ans;
+        solve(root, cnt, ans, k);
+        return ans;
     }
 };
