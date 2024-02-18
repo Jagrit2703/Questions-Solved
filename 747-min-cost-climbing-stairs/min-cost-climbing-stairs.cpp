67a2016d4b7293b1e942ba9e194a384f659a2e7a
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-02-18 15:41:58 +0530

    Time: 10 ms (18.27%) | Memory: 16.4 MB (19.95%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& cost, int n, vector<int>&dp){
-        if(n == 0) return cost[0];
-        if(n == 1) return cost[1];
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = cost[n] + min(solve(cost,n-1,dp), solve(cost,n-2,dp));
-        return dp[n];
-    }
+int ways(int n,vector<int>&cost,vector<int>&dp)
+{
+    if(n==0) return cost[n];
+    if(n==1) return cost[n];
+
+    if(dp[n] != -1) return dp[n]; 
+    int onestep=ways(n-1,cost,dp)+cost[n];
+    int twostep=ways(n-2,cost,dp)+cost[n];
+
+    dp[n]=min(onestep,twostep);
+    return dp[n];
+}
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int>dp(n+1,-1);
-        int ans = min(solve(cost,n-1,dp),solve(cost,n-2,dp));
-        return ans;
+        int n=cost.size();
+        vector<int>dp(n,-1);
+        int one=ways(n-1,cost,dp);
+        int two=ways(n-2,cost,dp);
+        return min(one,two);
     }
 };
