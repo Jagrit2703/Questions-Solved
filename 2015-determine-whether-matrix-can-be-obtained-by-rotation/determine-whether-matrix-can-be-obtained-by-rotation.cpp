bc2ca3d4f342465d02384eabf10a8eb9f8eb6c67
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-05-14 23:11:32 +0530

    Time: 4 ms (62.97%) | Memory: 13.9 MB (17.29%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-        if (mat == target) {
-            return true;
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }    
         }
-        int n = mat.size();
-        int deg = 3;
-        while (deg--) {
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    swap(mat[i][j], mat[j][i]);
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                reverse(mat[i].begin(), mat[i].end());
-            }
-            if(mat == target){
-                return true;
-            }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat==target) return true;
+        for(int i=1;i<=3;i++){
+            rotate(mat);
+            if(mat==target) return true;
         }
         return false;
     }
