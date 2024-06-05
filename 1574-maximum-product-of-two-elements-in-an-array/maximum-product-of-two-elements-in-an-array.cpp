8078d00cec240dec88166c51b33499bb967211bf
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-05 22:20:14 +0530

    Time: 4 ms (66.23%) | Memory: 13.8 MB (5.84%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // brute mei use two pointers i and j 
-        // array sorted ? n-1 & n-2 product : sort
-        // contains only positive ?
-        int ans = 0;
-        int n = nums.size();
-        if(nums.size()==1){
-            return nums[0];
+        priority_queue<int> maxHeap;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            maxHeap.push(nums[i]);
         }
-        sort(nums.begin(),nums.end());
-        for(int i = n-1; i>=0; i--){
-            ans = (nums[n-1]-1)*(nums[n-2]-1);
-        } 
-        return ans;
+
+        int x = maxHeap.top();
+        maxHeap.pop();
+        int y = maxHeap.top();
+        return (x-1)*(y-1);
     }
 };
