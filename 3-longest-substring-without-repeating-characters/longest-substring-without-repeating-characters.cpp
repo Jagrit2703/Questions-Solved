c7543856f470408e22c01460040106971487885c
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-17 22:28:42 +0530

    Time: 6 ms (84.74%) | Memory: 10.4 MB (77.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> st;
-        int maxi = 0;
-        int start = 0;
-        int end = 0;
-        while (start < s.size()) {
-            if (st.find(s[start]) == st.end()) {
-                //creating window
-                if (start - end + 1 > maxi)
-                    maxi = start - end + 1;
-                st.insert(s[start]);
-                start++;
+        vector < int > mpp(256, -1);
 
-            } else {
-                st.erase(s[end]);
-                end++;
-            }
-        }
-        return maxi;
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+      }
+      return len;
     }
 };
