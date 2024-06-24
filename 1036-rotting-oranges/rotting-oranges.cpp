2f80847390a9dbc8efd57477870a77a79974ccd3
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-24 20:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (99.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& grid, int i, int j, int min = 2) {
+        if (i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size() or
+            grid[i][j] == 0 or (1 < grid[i][j] and grid[i][j] < min))
+            return;
+        grid[i][j] = min;
+        dfs(grid, i + 1, j, min + 1);
+        dfs(grid, i - 1, j, min + 1);
+        dfs(grid, i, j + 1, min + 1);
+        dfs(grid, i, j - 1, min + 1);
+    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        queue<pair<pair<int,int>,int>>q;
-        int vis[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j] = 2;
-                }
-                else{
-                    vis[i][j] = 0;
-                }
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2)
+                    dfs(grid, i, j);
             }
         }
-    int tm = 0;
-    int drow[4] = {-1,0,+1,0};
-    int dcol[4] = {0,1,0,-1};
-    while(!q.empty()){
-        int r = q.front().first.first;
-        int c = q.front().first.second;
-        int t = q.front().second;
-        q.pop();
-        tm = max(tm,t);
-        for(int i = 0;i<4;i++){
-            int nrow = r + drow[i];
-            int ncol = c + dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] !=2 && grid[nrow][ncol] == 1){
-                q.push({{nrow,ncol},t+1});
-                vis[nrow][ncol]=2;
+        int min = 2;
+        for (auto& i : grid) {
+            for (auto& j : i) {
+                if (j == 1)
+                    return -1;
+                min = max(min, j);
             }
         }
-    }
-    for(int i = 0;i<n;i++){
-        for(int j = 0;j<m;j++){
-            if(vis[i][j] != 2 && grid[i][j] == 1) return -1;
-        }
-    }
-    return tm; 
+        return min - 2;
     }
 };
