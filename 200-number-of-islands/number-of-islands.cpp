c4b46ef76515f5a7aa3d3a48ceab88db27ddb1b4
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-24 19:15:49 +0530

    Time: 30 ms (49.19%) | Memory: 15.7 MB (93.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& grid, int i, int j){
-        int m = grid.size(), n = grid[0].size();
-        if(i>=0 && i<m && j>=0 && j<n && grid[i][j] =='1' ) return true;
-        return false;
-    }
     void eraseIslands(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
+            return;
+        }
         grid[i][j] = '0';
-        if(isValid(grid,i-1,j))
-            eraseIslands(grid, i - 1, j);
-        if(isValid(grid,i+1,j))
-            eraseIslands(grid, i + 1, j);
-        if(isValid(grid,i,j-1))
-            eraseIslands(grid, i, j - 1);
-        if(isValid(grid,i,j+1))
-            eraseIslands(grid, i, j + 1);
+        eraseIslands(grid, i - 1, j);
+        eraseIslands(grid, i + 1, j);
+        eraseIslands(grid, i, j - 1);
+        eraseIslands(grid, i, j + 1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
