928c83a2214d925dbdb98e9cae1b5616f7f75d01
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-27 13:52:35 +0530

    Time: 4 ms (31.80%) | Memory: 12.4 MB (10.26%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-        cols = matrix[0].size();
-        int row = 0;
-        int col = cols-1;
-
-        while(row<rows && col>-1){
-            int curr = matrix[row][col];
-            if(curr == target)return true;
-            if(target>curr)row++;
-            else col--;
+    void DFS(vector<vector<int>>& matrix, int target, int i, int j, vector<vector<bool>>& visited, bool& ans) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || matrix[i][j] != target) {
+            return;
         }
-        return false;
+        if(matrix[i][j] == target) {
+            ans = true;
+            return;
+        }
+        visited[i][j] = true;
+        DFS(matrix, target, i + 1, j, visited, ans);
+        DFS(matrix, target, i - 1, j, visited, ans);
+        DFS(matrix, target, i, j + 1, visited, ans);
+        DFS(matrix, target, i, j - 1, visited, ans);
+        visited[i][j] = false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool ans = false;
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == target) {
+                    ans = true;
+                    break;
+                }
+                DFS(matrix, target, i, j, visited, ans);
+                if (ans) return ans;
+            }
+        }
+        return ans;
     }
 };
