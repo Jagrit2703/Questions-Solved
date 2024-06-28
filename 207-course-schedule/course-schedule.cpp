c5993d2a893f7f2bcc31122024c9f22d45f40f6d
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-06-28 09:58:00 +0530

    Time: 14 ms (74.93%) | Memory: 19.6 MB (8.83%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,30 +1,36 @@
-    // BFS METHOD
 class Solution {
 public:
+    bool DFS(unordered_map<int,vector<int>>&graph,int u,vector<bool>&vis,vector<bool>&inRec){
+        vis[u]=true;
+        inRec[u]= true;
+        for(auto i : graph[u]){
+            if(vis[i]==false && DFS(graph,i,vis,inRec)){
+                return true;
+            }
+            else if(inRec[i]==true){
+                return true;
+            }
+        }
+        inRec[u]=false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>ad[numCourses];
-        vector<int>inDegree(numCourses,0);
-        for(auto i:prerequisites){
-            ad[i[1]].push_back(i[0]);
-            inDegree[i[0]]++;
+        unordered_map<int,vector<int>>graph; 
+        vector<bool>vis(numCourses, false);
+        vector<bool>inRec(numCourses, false);
+
+        for(auto vec : prerequisites){
+            int u = vec[0];
+            int v = vec[1];
+
+            graph[v].push_back(u);
         }
-        queue<int>q;
-        int count=0;
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0){
-             q.push(i);
-             count++;
+
+        for(int i = 0; i<numCourses; i++){
+            if(vis[i]==false && DFS(graph,i,vis,inRec)){
+                return false;
             }
         }
-        while(q.size()){
-            int x = q.front();q.pop();
-            for(auto i:ad[x]){
-                inDegree[i]--;
-                if(inDegree[i]==0){
-                    q.push(i);count++;
-                }
-            }
-        }
-        return count == numCourses;
+        return true;
     }
 };
