50c64f048bc9139045e9f8603fd84f060ad35bc3
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-07-07 23:51:19 +0530

    Time: 143 ms (34.45%) | Memory: 59.4 MB (88.20%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& land, int i, int j, vector<int>& temp) {
-        int n = land.size();
-        int m = land[0].size();
-        if (i < 0 || i == n || j < 0 || j == m || land[i][j] == 0) {
+    void dfs(vector <vector <int>> &land, int i, int j, int &r, int &c) {
+        if (i < 0 || j < 0 || i >= land.size() || j >= land[0].size() || land[i][j] == 0) {
             return;
         }
-        if (i > temp[2]) {
-            temp[2] = i;
-            temp[3] = j;
-        } else if (i == temp[2] && j > temp[3]) {
-            temp[3] = j;
-        }
         land[i][j] = 0;
-        DFS(land, i + 1, j, temp);
-        DFS(land, i - 1, j, temp);
-        DFS(land, i, j + 1, temp);
-        DFS(land, i, j - 1, temp);
+        r = max(r, i);
+        c = max(c, j);
+        dfs(land, i + 1, j, r, c);
+        dfs(land, i, j + 1, r, c);
     }
+
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        vector<vector<int>> coordinates;
-        int n = land.size();
-        int m = land[0].size();
-        vector<vector<bool>> vis;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        vector <vector <int>> ans;
+        for (int i = 0; i < land.size(); i++) {
+            for (int j = 0; j < land[0].size(); j++) {
                 if (land[i][j] == 1) {
-                    vector<int> temp = {i, j, i, j};
-                    DFS(land, i, j, temp);
-                    coordinates.push_back(temp);
+                    int r = 0;
+                    int c = 0;
+                    dfs(land, i, j, r, c);
+                    ans.push_back({i, j, r, c});
                 }
             }
         }
-        return coordinates;
+        return ans;
     }
 };
