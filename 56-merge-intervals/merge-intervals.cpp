21f8450bca655f3fcd0574b7106d883d89d3518b
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-07-08 14:49:34 +0530

    Time: 20 ms (77.40%) | Memory: 22.7 MB (41.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
-        if (intervals.size() == 1) {
-            return intervals;
-        }
-        vector<pair<int, int>> p;
-        for (int i = 0; i < intervals.size(); i++) {
-            p.push_back({intervals[i][0], intervals[i][1]});
-        }
-        sort(p.begin(), p.end());
-        int a = p[0].first;
-        int b = p[0].second;
-        for (int i = 0; i < p.size() - 1; i++) {
-            vector<int> temp(2);
-            if (b >= p[i + 1].first) {
-                b = max(b, p[i + 1].second);
-            } else {
-                temp[0] = a;
-                temp[1] = b;
-                a = p[i + 1].first;
-                b = p[i + 1].second;
-                ans.push_back({temp});
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+    /*
+    run a for loop check i ka 2nd and j ka first if i ka second is bigger than j ka first then it overlaps and u can merge it into one
+    to merge u only append i ka first and j ka second
+    */
+     vector<vector<int>> result;
+        if(interval.size()==0)return result;
+        sort(interval.begin(),interval.end());
+        result.push_back(interval[0]);
+        int j=0;
+        for(int i=1;i<interval.size();i++)
+        {
+            if(result[j][1]>=interval[i][0]) result[j][1]=max(result[j][1],interval[i][1]);
+            else
+            {
+                j++;
+                result.push_back(interval[i]);
             }
         }
-        ans.push_back({a, b});
-        return ans;
+        return result;
     }
 };
