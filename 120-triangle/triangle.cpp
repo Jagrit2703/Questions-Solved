f6987be6b86c2b9bcc99eebe0d633f21d4a64be1
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-07-11 21:05:03 +0530

    Time: 2 ms (87.72%) | Memory: 11.7 MB (5.46%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int helper(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>&dp){
+        if(i == triangle.size()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = triangle[i][j]+min(helper(triangle,i+1,j,dp),helper(triangle,i+1,j+1,dp));
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int minSum = INT_MAX;
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        minSum = helper(triangle,0,0,dp);
+        return minSum;
+    }
+};
