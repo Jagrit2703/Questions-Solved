0fa169307d38d55d44132df09d513d8870d0a8a8
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-07-16 15:01:04 +0530

    Time: 8 ms (48.55%) | Memory: 29.1 MB (5.12%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        vector<int>ans;
-        priority_queue<int, vector<int>, greater<int>>q;
-        for(auto &i : nums){
+        priority_queue<int,vector<int>,greater<int>>q;
+        for(int i : nums){
             q.push(i);
         }
-        while(q.size()){
-            int first = q.top();
+        vector<int>ans;
+        while(!q.empty()){
+            int alice = q.top();
             q.pop();
-            int second = q.top();
+            int bob = q.top();
             q.pop();
-            ans.push_back(second);
-            ans.push_back(first);
+            ans.push_back(bob);
+            ans.push_back(alice);
         }
         return ans;
     }
