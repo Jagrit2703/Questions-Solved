f012121a87f8c21ef19997da27f76e5aea9700c6
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-08-09 17:45:43 +0530

    Time: 3 ms (80.28%) | Memory: 10 MB (53.89%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<string>>res;
-    unordered_set<int>diag;
-    unordered_set<int>antidiag;
-    unordered_set<int>cols;
     int N;
+    bool isValid(vector<string>&board, int row, int col){
+        for(int i = row; i>=0; i--){
+            if(board[i][col]=='Q')return false;
+        }
+        //left diagonal check
+        for(int i = row, j = col; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q')return false;
+        }
+        //right diagonal check
+        for(int i = row, j = col; i>=0 && j<N; i--, j++){
+            if(board[i][j]=='Q')return false;
+        }
+        return true;
+    }
     void search(vector<string>&board, int row){
         if(row>=N){
             res.push_back(board);
             return;
         }
         for(int col = 0; col<N; col++){
-            int diagC = row+col;
-            int antiDiagC = row-col;
-            if(cols.find(col)!=cols.end() || diag.find(diagC)!=diag.end() || antidiag.find(antiDiagC)!=antidiag.end()){
-                continue;
+            if(isValid(board,row,col)){
+                board[row][col] = 'Q';
+                search(board,row+1);
+                board[row][col] = '.';
             }
-            cols.insert(col);
-            diag.insert(diagC);
-            antidiag.insert(antiDiagC);
-            board[row][col] = 'Q';
-
-            search(board,row+1);
-
-            cols.erase(col);
-            diag.erase(diagC);
-            antidiag.erase(antiDiagC);
-            board[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         N = n;
+        if(n<=0)return {{}};
         vector<string>board(n,string(n,'.'));
         search(board,0);
         return res;
