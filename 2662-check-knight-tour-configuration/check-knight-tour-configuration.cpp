96a78887b3bf8e7de7b3aa09f19bba4c8e86373b
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-08-11 14:40:40 +0530

    Time: 14 ms (39.06%) | Memory: 31.5 MB (58.37%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool solve(int i, int j, int k, int &m, int &n, vector<vector<int>>&g){
+        //valid moves = 2 horizontal 1 vertical
+        //valid moves = 2 vertical 1 horizontal 
+        // create visited and mark them, create moves as 
+        //solve(i+2,j+1), solve(i+1,j+2), solve(i+2,j-1), solve(i+1,j-2), solve(i-2,j-1)
+        if(i<0||j<0||i>=m||j>=n||g[i][j] != k)return false;
+        if(k==m*n-1)return true;
+        return solve(i+2,j-1,k+1,m,n,g)||solve(i+2,j+1,k+1,m,n,g)||solve(i-2,j-1,k+1,m,n,g)||solve(i-2,j+1,k+1,m,n,g)||solve(i-1,j-2,k+1,m,n,g)||solve(i-1,j+2,k+1,m,n,g)||solve(i+1,j+2,k+1,m,n,g)||solve(i+1,j-2,k+1,m,n,g);
+        
+    }
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        int x = 0,m=grid.size(),n=grid[0].size();
+        return solve(0,0,0,m,n,grid);
+    }
+};
