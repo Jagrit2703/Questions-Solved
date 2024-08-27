6d65f39f3e20304d69930571714c636c02a8e716
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-08-27 12:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (39.82%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int maxRepeating(string a, string b) {
-        string s = b + "#" + a;
-        int n = s.size(), nb = b.size();
-
-        vector<int> lps(n + 1, 0);
-        lps[0] = -1;
-        
-        int i = 0, j = -1;
-
-        while(i < n){
-            while(j != -1 and s[i] != s[j]) j = lps[j];
-            lps[++i] = ++j;
-        }
-        
-        int ans = 0;
-        for(int i = 0; i <= n; i++){
-            if(lps[i] == nb){
-                int len = 0, j = i;
-                while(j <= n and lps[j] == nb) i = j, j += nb, len++;
-                ans = max(ans, len);
+    int maxRepeating(string sequence, string word) {
+        int m=sequence.size();
+        int n=word.size();
+        int maxi=INT_MIN;
+        int ans=0;
+        for(int i=0;i<=m-n;i++){
+            if(sequence.substr(i,n)==word){
+                ans++;
+                i+=(n-1);
+            }
+            else {
+                if(ans){
+                maxi=max(ans,maxi);
+                i-=n-1;
+                ans=0;
+                }
             }
         }
-        return ans;
+        maxi=max(ans,maxi);
+        return maxi;
     }
 };
