fdf8c356b7280073d09e8af1981e23d2aa41e4ac
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-08-27 12:58:46 +0530

    Time: 3 ms (42.77%) | Memory: 8 MB (63.06%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-        int m=sequence.size();
-        int n=word.size();
-        int maxi=INT_MIN;
-        int ans=0;
-        for(int i=0;i<m;i++){
-            if(sequence.substr(i,n)==word){
-                ans++;
-                i+=(n-1);
-            }
-            else {
-                if(ans){
-                maxi=max(ans,maxi);
-                i-=n-1;
-                ans=0;
-                }
-            }
+        int count=0;
+        string temp=word;
+        while(sequence.find(temp)!=string::npos)
+        {
+            temp+=word;
+            count++;
         }
-        maxi=max(ans,maxi);
-        return maxi;
+        return count;
     }
 };
