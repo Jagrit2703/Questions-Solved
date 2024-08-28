51278c2583a1780f4186e9203771aa86383799a1
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-08-29 00:46:22 +0530

    Time: 31 ms (6.50%) | Memory: 244.5 MB (5.12%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-int ways(int n,vector<int>&cost,vector<int>&dp)
-{
-    if(n==0) return cost[n];
-    if(n==1) return cost[n];
-
-    if(dp[n] != -1) return dp[n]; 
-    int onestep=ways(n-1,cost,dp)+cost[n];
-    int twostep=ways(n-2,cost,dp)+cost[n];
-
-    dp[n]=min(onestep,twostep);
-    return dp[n];
-}
+// ek rec call starting from 0 and ek call from 1 and find min of those
+int check(int i, vector<int>cost,int n,  vector<int>&dp){
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int one_climb = cost[i] + check(i + 1, cost, n , dp );
+        int two_climb = cost[i] + check(i + 2, cost, n , dp);
+        dp[i] = min(one_climb, two_climb);
+        return dp[i];
+    }
+public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int>dp(n,-1);
-        int one=ways(n-1,cost,dp);
-        int two=ways(n-2,cost,dp);
-        return min(one,two);
+        int n = cost.size();
+        vector<int>dp(n + 1, -1);
+        int start_from_0th_step = check(0, cost,n, dp ); 
+        int start_from_1th_step = check(1, cost,n , dp );
+        int ans =    min(start_from_0th_step , start_from_1th_step);
+        return ans; 
     }
 };
