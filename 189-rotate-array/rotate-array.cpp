7139b51bd95af0682f35c9c04aa9d08468f4c8fa
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-09-17 02:39:39 +0530

    Time: 25 ms (22.81%) | Memory: 28.3 MB (24.65%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,15 +3,14 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        if (k == 0)
-            return;
-        vector<int> ans;
-        for (int i = n - k; i < n; i++) {
-            ans.push_back(nums[i]);
+        vector<int> rotated(n);
+
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
         }
-        for (int i = 0; i < n - k; i++) {
-            ans.push_back(nums[i]);
-        }
-        nums = ans;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotated[i];
+        }        
     }
 };
