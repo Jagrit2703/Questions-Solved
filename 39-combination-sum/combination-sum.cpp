abbfb8bb40889cc65b82e485bcd489e83ffd8e04
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-09-18 18:14:03 +0530

    Time: 3 ms (79.51%) | Memory: 12.8 MB (50.83%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        /*
-        1.can memoization be applied ?
-        2.recursive call karo to check and add the number, once the sum==target push them back into a vector<vector<int>>;
-        */
-        vector<vector<vector<int>>> dp(target + 1);
-        for (auto &num: candidates) {
-            for (int i = 1; i <= target; i++) {
-                if (i == num) {
-                    dp[i].push_back({num});
-                } else if (i > num) {
-                    for (auto vec: dp[i - num]) {
-                        vec.push_back(num);
-                        dp[i].push_back(vec);
-                    }
-                }
-            }
+    vector<vector<int>>ans;
+    void solve(vector<int>& candidates, int target,int ind,int total,vector<int>&temp){
+        if(total==target){
+            ans.push_back(temp);
+            return;
         }
-
-        return dp[target];
+        if(total>target || ind>=candidates.size()) return;
+        temp.push_back(candidates[ind]);
+        solve(candidates,target,ind,total+candidates[ind],temp);
+        temp.pop_back();
+        solve(candidates,target,ind+1,total,temp);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        int n = candidates.size();
+        solve(candidates,target,0,0,temp);
+        return ans;
     }
 };
