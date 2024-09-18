220ae0ecd11b929e4c037991410c85e40761d926
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2024-09-18 18:56:30 +0530

    Time: 4 ms (43.85%) | Memory: 15.8 MB (27.03%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
+    vector<string>ans;
+    void generateString(int n, int left, int right,string temp){
+        if(left==right && left+right==n*2){
+            ans.push_back(temp);
             return;
         }
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+        if(left<n){
+            generateString(n,left+1,right,temp+"(");
         }
-        else if(open == 0){
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
+        if(right<left){
+            generateString(n,left,right+1,temp+")");
         }
-        else if(close == 0){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
-        }
+
     }
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
+        string temp = "";
+        generateString(n,0,0,temp);
         return ans;
     }
 };
